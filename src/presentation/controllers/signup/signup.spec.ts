1675a4862ea40550abbb6b0a0d272fe2634f56ec
rodnikerson <rodolpho.oliveira@acct.global>
2024-08-04 12:40:07 -0300

    refactor: move all signup protocols to a single file

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -1,8 +1,11 @@
-import { AddAccount, AddAccountModel } from "@/domain/usecases/add-account"
-import { InvalidParamError, MissingParamError, ServerError } from "../errors"
-import { EmailValidator } from "../protocols"
+import { InvalidParamError, MissingParamError, ServerError } from "../../errors"
 import { SignUpController } from "./signup"
-import { AccountModel } from "@/domain/models/account"
+import {
+  EmailValidator,
+  AccountModel,
+  AddAccount,
+  AddAccountModel,
+} from "./signup-protocols"
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {

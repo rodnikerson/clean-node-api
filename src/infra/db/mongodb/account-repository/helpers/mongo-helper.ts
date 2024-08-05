c0af2aea863131ec42c6f62d0fc44a039386d9a2
rodnikerson <nikersonrodolpho@gmail.com>
2024-08-05 16:27:47 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/account-repository/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/account-repository/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from "mongodb"
+import { Collection, InsertOneResult, MongoClient, ObjectId } from "mongodb"
 
 export const MongoHelper = {
   client: null as unknown as MongoClient | null,
@@ -19,5 +19,35 @@ export const MongoHelper = {
       await this.client.close()
       this.client = null
     }
+  },
+
+  async getCollection(name: string): Promise<Collection> {
+    if (!this.client?.db) {
+      this.client = await MongoClient.connect(this.uri)
+    }
+    return this.client.db().collection(name)
+  },
+
+  mapAdd<T>(result: InsertOneResult<Document>, data: any): T {
+    if (data._id) {
+      delete data._id
+    }
+    return Object.assign({}, data, { id: result.insertedId.toHexString() })
+  },
+
+  map(collection: any): any {
+    if (
+      !collection ||
+      collection.length === 0 ||
+      Object.keys(collection).length === 0
+    ) {
+      return null
+    }
+    const { _id, ...collectionWithoutId } = collection
+    return Object.assign({}, collectionWithoutId, { id: _id.toHexString() })
+  },
+
+  transformIdInObjectId(id: string): ObjectId {
+    return new ObjectId(id)
   },
 }
